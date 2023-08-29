1f172044831887a7541c8bd273e567872674634b
joao guilherme <joaoguilhermehig@gmail.com>
2023-08-29 17:58:49 -0300

    BOTTOM NAVIGATION COLOCADO

--- App.tsx
+++ App.tsx
@@ -1,32 +1,22 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import AppContacts from './src/pages/AppContacts/';
-import Contacts from './src/pages/Contacts/'
-import Information from './src/pages/Information/'
+import CameraComponent from './src/components/CameraComponent';
+import HomeComponent from './src/components/HomeComponent'
 
-const Stack = createStackNavigator()
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
-function Tabs() {
+function MyTabs() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="AppContacts" component={AppContacts} />
-            <Tab.Screen name="Contacts" component={Contacts} />
+            <Tab.Screen name="Home" component={HomeComponent} />
+            <Tab.Screen name="Settings" component={CameraComponent} />
         </Tab.Navigator>
-    )
+    );
 }
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="AppContacts" component={Tabs} />
-                <Stack.Screen name="Information" component={Information} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
-
+        <MyTabs />
+    );
+};
